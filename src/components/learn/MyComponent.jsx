fda5731e8ad145f4ae70410fda7f7bfcc220cf8e
NguyenDoKhanh <khanhndhe182074@fpt.edu.vn>
2026-01-02 02:43:16 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,20 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const name = "khanh"; //string
+    // const name = 21; //number
+    // const name = true; //boolean
+    // const name = undefined;
+    // const name = null;
+    const name = [1, 2, 3];
+    // const name = {
+    //     name: "khanh",
+    //     age: 25
+    // }
     return (
         <>
-            <div>khanh va jayce</div>
+            <div>{JSON.stringify(name)} va jayce</div>
+            <div>{console.log("JAYCE")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}
             >child</div>
